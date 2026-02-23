fd915a92e85bbef77b4fc16e6075dc1f3c0ad8dd
ionelDumitru <ionel.dumitru04@e-uvt.ro>
2026-02-23 20:31:07 +0200

    Test webhook

--- jenkins-jobs/test.groovy
+++ jenkins-jobs/test.groovy
@@ -2,7 +2,7 @@ job('my-simple-job') {
     description('Un job simplu creat cu DSL')
     label("agent")
     steps {
-        shell('echo "Hello from DSL job!! After webhook and seed "')
+        shell('echo "Hello from DSL job!! After webhook and seed  "')
     }
 
 }
